4f2b422e60303c2982654be5ed4e4a82968012ad
sunilnarwaria <41354869+sunilnarwaria@users.noreply.github.com>
2021-03-01 08:08:34 +0530

    Update LPS.cpp

--- LPS.cpp
+++ LPS.cpp
@@ -1,4 +1,4 @@
-
+// Pseudo code
 int i = 1,len = 0;
 while(i<str.size(){
    if(s[i] == s[len]){
@@ -16,3 +16,71 @@ while(i<str.size(){
   }
 }
       
+// Code
+       // Efficient CPP program to find length of 
+// the longest prefix which is also suffix
+#include<bits/stdc++.h>
+using namespace std;
+
+// Returns length of the longest prefix
+// which is also suffix and the two do
+// not overlap. This function mainly is
+// copy computeLPSArray() of in below post
+// https://www.geeksforgeeks.org/searching-for-patterns-set-2-kmp-algorithm/
+int longestPrefixSuffix(string s)
+{
+	int n = s.length();
+
+	int lps[n];
+	lps[0] = 0; // lps[0] is always 0
+
+	// length of the previous
+	// longest prefix suffix
+	int len = 0;
+
+	// the loop calculates lps[i]
+	// for i = 1 to n-1
+	int i = 1;
+	while (i < n)
+	{
+		if (s[i] == s[len])
+		{
+			len++;
+			lps[i] = len;
+			i++;
+		}
+		else // (pat[i] != pat[len])
+		{
+			// This is tricky. Consider
+			// the example. AAACAAAA
+			// and i = 7. The idea is
+			// similar to search step.
+			if (len != 0)
+			{
+				len = lps[len-1];
+
+				// Also, note that we do
+				// not increment i here
+			}
+			else // if (len == 0)
+			{
+				lps[i] = 0;
+				i++;
+			}
+		}
+	}
+
+	int res = lps[n-1];
+
+	// Since we are looking for
+	// non overlapping parts.
+	return (res > n/2)? n/2 : res;
+}
+
+// Driver program to test above function
+int main()
+{
+	string s = "abcab";
+	cout << longestPrefixSuffix(s);
+	return 0;
+}
