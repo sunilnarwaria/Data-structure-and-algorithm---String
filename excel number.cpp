1e0e5fc5326d44b26701643dff37b22d990cee83
sunilnarwaria <41354869+sunilnarwaria@users.noreply.github.com>
2021-03-02 09:15:11 +0530

    Create excel number.cpp

---
+++ excel number.cpp
@@ -0,0 +1,11 @@
+// interviewbit
+// result mai 26 ka multiply is key point
+
+nt Solution::titleToNumber(string s) {
+     int result = 0;
+       for (int i = 0; i < s.size(); i++) {
+               result = result * 26 + (s[i] - 'A' + 1);
+       }
+       return result;
+    
+}
